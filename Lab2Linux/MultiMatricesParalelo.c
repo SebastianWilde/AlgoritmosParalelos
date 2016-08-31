863a3604b86a14c52029ad90256c52a60fec81eb
Sebastian Alarcon <sebastian.alarcon@ucsp.edu.pe>
2016-08-30 23:06:51 -0500

    Correciones

--- Lab2Linux/MultiMatricesParalelo.c
+++ Lab2Linux/MultiMatricesParalelo.c
@@ -1,14 +1,8 @@
-/* Programa de multiplicación de matrices paralelizado,
-   utilizando MPI.  2001, Daniel Manrique. */
-
 #include "mpi.h"
 #include <stdio.h>
 #include <math.h>
 #include <stdlib.h>
 #include <unistd.h>
-
-/*Funciones de matrices*/
-
 void LlenarMatriz(int * matriz, int col, int fila)
 {
 	for (int i=0;i<(fila*col);i++) 
@@ -17,7 +11,6 @@ void LlenarMatriz(int * matriz, int col, int fila)
     }
     return;
 }
-
 void MostrarMatriz(int *matriz, int fila, int col)
 {
     for (int i = 0; i < fila; i++) 
@@ -30,7 +23,6 @@ void MostrarMatriz(int *matriz, int fila, int col)
     }
     return;
 }
-
 int main(int argc, char **argv)
 {
     int rank, comm_sz, rv, fila, col;
@@ -45,22 +37,12 @@ int main(int argc, char **argv)
     int dimension;
     MPI_Status status;
 	dimension=10;
-    // Inicializar mpi
     MPI_Init(&argc, &argv);
-
-    /* determinar numero total de procesos y cual
-       somos, asi como en que procesador (nodo) estamos
-       corriendo. */
     MPI_Comm_size(MPI_COMM_WORLD, &comm_sz);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     fila = dimension;
     col = dimension;
-
-    /* En MPI el proceso inicial (padre) tiene rank
-       de 0, esta es la sección donde el proceso padre
-       realiza su trabajo */
     if (rank == 0) {
-		// Asignar memoria para tres matrices de de fila
 		matriz1 = malloc(fila * col * sizeof(int));
 		matriz2 = malloc(fila * col * sizeof(int));
 		matriz3 = malloc(fila * col * sizeof(int));
@@ -74,17 +56,12 @@ int main(int argc, char **argv)
 		}
 		LlenarMatriz(matriz1,fila,col);
 		LlenarMatriz(matriz2,fila,col);
-		/*Determinar el momento de inicio de ejecución*/
 		startwtime = MPI_Wtime();
 		printf("Primera matriz 1\n");
 		MostrarMatriz(matriz1,fila,col);
 		printf("\nPrimera matriz 2\n");
 		MostrarMatriz(matriz2,fila,col);
 		completefilas = 0;
-		/* el proceso con rank 0 transmite las dos
-		   matrices a los demás, esto se hace por medio
-		   de un broadcast de MPI a todos los miembros de
-		   mi comunicador (MPI_COMM_WORLD) */
 		rv = MPI_Bcast(matriz1, fila * col, MPI_INT, 0, MPI_COMM_WORLD);
 		printf("Root, Broadcast said %d\n", rv);
 		rv = MPI_Bcast(matriz2, fila * col, MPI_INT, 0, MPI_COMM_WORLD);
@@ -92,7 +69,6 @@ int main(int argc, char **argv)
 		resultfila = malloc((col + 1) * sizeof(int));
 		while (completefilas < fila) 
 		{
-		    /* MPI_ANY_SOURCE se admiten valores de cualquier proceso. */
 		    MPI_Recv(resultfila, col + 1, MPI_INT, MPI_ANY_SOURCE, 1, MPI_COMM_WORLD, &status);
 		    printf("recibido renglon %d de %d\n",resultfila[0], status.MPI_SOURCE);
 		    completefilas++;
@@ -101,7 +77,6 @@ int main(int argc, char **argv)
 					matriz3[resultfila[0]*col+i]=resultfila[i+1];
 			    }
 		}
-		// anotar tiempo al
 		endwtime = MPI_Wtime();
 		printf("\nPrimera matriz 3\n");
 		MostrarMatriz(matriz3,fila,col);
@@ -122,24 +97,12 @@ int main(int argc, char **argv)
 		    if (matriz3!=NULL) free(matriz3);
 		    exit(1);
 		}
-		/* En MPI, si mi rank no es 0, una llamada al
-		   broadcast (notar el parametro 4 que es de 0)
-		   indica recibir el broadcast del proceso con ese
-		   rank. */
 		rv = MPI_Bcast(matriz1, fila * col,MPI_INT, 0, MPI_COMM_WORLD);
 		rv = MPI_Bcast(matriz2, fila * col,MPI_INT, 0, MPI_COMM_WORLD);
-
-		/*de acuerdo a las dimensiones de las matrices
-		   y al numero de procesos, calcular numero de
-		   particiones, renglones por particion, el
-		   primer renglon que tiene que resolver este
-		   proceso, y el ultimo renglon.  */
 		partitions = comm_sz - 1;
 		filastodo = (int) (fila / partitions);
 		firstfila = filastodo * (rank - 1);
 		lastfila = firstfila + filastodo - 1;
-		// el ultimo proceso amplia su limite para
-		// tomar los huerfanitos
 		if (rank == comm_sz - 1) {
 		    lastfila = lastfila + (fila % partitions);
 		}
