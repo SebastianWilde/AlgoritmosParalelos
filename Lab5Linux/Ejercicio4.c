69163ede1b966bbce64d8183ff150f77d0bc0840
Sebastian Alarcon <sebastian.alarcon@ucsp.edu.pe>
2016-09-21 11:39:22 -0500

    Ejercicio 6

--- Lab5Linux/Ejercicio4.c
+++ Lab5Linux/Ejercicio4.c
@@ -9,27 +9,18 @@ double timeval_diff(struct timeval *a, struct timeval *b)
     (double)(a->tv_sec + (double)a->tv_usec/1000000) -
     (double)(b->tv_sec + (double)b->tv_usec/1000000);
 }
-void llenarM(double *matrix,int fila, int columna)
-{
-    for (int i=0;i<(fila*columna);i++) 
-    {
-		*(matrix + i)=1 + (double)(10*random()/(RAND_MAX+1.0));
-    }
 
-}
-int filas=80;
-int columnas=80;
 int thread_count;
-struct timeval t_ini1, t_fin1;
-struct timeval t_ini2, t_fin2;
-double media1=0;
-double media2=0;
-void* tiempo_H(void* rango)
+struct timeval t_ini1, t_fin1, t_ini2, t_fin2;
+double creacion;
+double destruccion;
+
+void* example(void* rango)
 {
 	gettimeofday(&t_fin1, NULL);
-	media1 += timeval_diff(&t_fin1, &t_ini1);
-	double* Matriz=malloc(filas*columnas*sizeof(double));
-	llenarM(Matriz,filas,columnas);
+	creacion += timeval_diff(&t_fin1, &t_ini1);
+	int x=5;
+	x+=1;
 	gettimeofday(&t_ini2, NULL);
 	return NULL;
 }
@@ -38,19 +29,20 @@ int main(int argc,char* argv[])
 
 	long thread;
 	pthread_t* thread_handles;
+	creacion=destruccion=0;
 	thread_count=strtol(argv[1],NULL,10);
 	thread_handles=malloc (thread_count*sizeof(pthread_t));
 	for(thread=0;thread<thread_count;thread++)
 	{
 		gettimeofday(&t_ini1, NULL);
-		pthread_create(&thread_handles[thread],NULL,tiempo_H,(void *)thread);
+		pthread_create(&thread_handles[thread],NULL,example,(void *)thread);
 		pthread_join(thread_handles[thread],NULL);
 		pthread_detach(thread_handles[thread]);
 		gettimeofday(&t_fin2, NULL);
-		media2 += timeval_diff(&t_fin2, &t_ini2);
+		destruccion += timeval_diff(&t_fin2, &t_ini2);
 	}
-	double secs1=media1/thread_count;
-	double secs2=media2/thread_count;
+	double secs1=creacion/thread_count;
+	double secs2=destruccion/thread_count;
   	printf("Tiempo de creacion %.16g milliseconds\n", secs1 * 1000.0);
   	printf("Tiempo de destruccion %.16g milliseconds\n", secs2 * 1000.0);
 	free(thread_handles);
